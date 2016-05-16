6535b7e5699fe2efe1db204c2a7830da8254e109
gen1321 <gen1321@mail.ru>
2016-05-16 15:05:04 +0300

    fix

--- s3upload.js
+++ s3upload.js
@@ -101,7 +101,7 @@ S3Upload.prototype.executeOnSignedUrl = function(file, callback) {
 };
 
 S3Upload.prototype.uploadToS3 = function(file, signResult) {
-    var xhr = this.createCORSRequest('PUT', signResult.url+'/test');
+    var xhr = this.createCORSRequest('PUT', signResult.url+'/assets/'+file.name);
     if (!xhr) {
         this.onError('CORS not supported', file);
     } else {
